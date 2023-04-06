07bd1a0bf2de8b5f64cc2463cf902881ea8816f4
Naila Thevenot <thevenotnaila@gmail.com>
2023-04-05 22:36:33 -0400

    sierpenski's triangle

--- Self-Similarity/sierpenski.js
+++ Self-Similarity/sierpenski.js
@@ -2,12 +2,20 @@ const SVG_NS = "http://www.w3.org/2000/svg";
 let counter = 0 
 initialization()
 
-//make one function that takes the different coordinae configurations
 function initialization(){
     let SVG = document.querySelector("svg")
+    let body = document.querySelector("body")
+
+    let res = document.createElement("h3")
+    res.innerHTML = "Sources: https://en.wikipedia.org/wiki/Sierpi%C5%84ski_triangle"
+    body.appendChild(res)
+    let des = document.createElement("h3")
+    des.innerHTML = "Used recursion to create the 3 inner triangles for each triangle using coordinate transformations"
+    body.appendChild(des)
+
     let mainTriangle = document.createElementNS(SVG_NS, "polygon")
-    let x = 600
-    let midpoint = 300
+    let x = 800
+    let midpoint = x/2
     mainTriangle.setAttributeNS(null, "points", `0,${x} ${midpoint},0 ${x},${x}`)
     mainTriangle.setAttributeNS(null, "fill", "grey")
     SVG.appendChild(mainTriangle)
@@ -15,40 +23,36 @@ function initialization(){
     //3 elements 
 
     let top = document.createElementNS(SVG_NS, "polygon")
-    top.setAttributeNS(null, "points", `${formula(0, 300)},${formula(600,0)} 300,0 ${formula(300,600)},${formula(0,600)}`) //md parent left, parent top, md parent right 
-    top.setAttributeNS(null, "fill", "black")
+    top.setAttributeNS(null, "points", `${formula(0,midpoint)},${formula(x,0)} ${midpoint},0 ${formula(midpoint,x)},${formula(0,x)}`) //md parent left, parent top, md parent right 
+    top.setAttributeNS(null, "fill", "grey")
     SVG.appendChild(top)
 
-    // let left = document.createElementNS(SVG_NS, "polygon")
-    // left.setAttributeNS(null, "points", `0,600 ${formula(0, 300)},${formula(600,0)} ${formula(0,600)},${formula(600,600)}`) //parent bottom left, midpoint of parent left, midpoint of parent bttom
-    // left.setAttributeNS(null, "fill", "black")
-    // SVG.appendChild(left)
+    let left = document.createElementNS(SVG_NS, "polygon")
+    left.setAttributeNS(null, "points", `0,${x} ${formula(0, midpoint)},${formula(x,0)} ${formula(0,x)},${formula(x,x)}`) //parent bottom left, midpoint of parent left, midpoint of parent bttom
+    left.setAttributeNS(null, "fill", "black")
+    SVG.appendChild(left)
 
-    // let right = document.createElementNS(SVG_NS, "polygon")
-    // right.setAttributeNS(null, "points", `${formula(0,600)},${formula(600,600)} ${formula(300,600)},${formula(0,600)} ${x},${x}`) //midpoint of parent bttom, md parent right, parent right bottom 
-    // right.setAttributeNS(null, "fill", "black")
-    // SVG.appendChild(right)
+    let right = document.createElementNS(SVG_NS, "polygon")
+    right.setAttributeNS(null, "points", `${formula(0,x)},${formula(x,x)} ${formula(midpoint,x)},${formula(0,x)} ${x},${x}`) //midpoint of parent bttom, md parent right, parent right bottom 
+    right.setAttributeNS(null, "fill", "black")
+    SVG.appendChild(right)
 
-    topTriangle(top,counter, 1)
-    leftTriangle(top, counter, 1)
-    rightTriangle(top, counter, 1)
+    let count = 5
+    three_triangles(top, counter, count)
+    three_triangles(left, counter, count)
+    three_triangles(right, counter, count)
 
     //create groups within each element 
 
 }
 
-function topTriangle(parent,counter,count){
-//should set parents color to grey 
-    if(counter == count + 1){
-        console.log(counter)
-        console.log(count)
+function three_triangles(parent,counter,count){
+    if(counter == count){
         return 
     }
 
-    let SVG = document.querySelector("svg")
     let points = parent.getAttributeNS(null, "points").split(" ")
     let coordinates = []
-    console.log(points)
     
     for (let index = 0; index < points.length; index++) {
         let coord = points[index].split(",")
@@ -56,78 +60,29 @@ function topTriangle(parent,counter,count){
         coordinates.push(parseInt(coord[1]))
         
     }
-
-    let element = document.createElementNS(SVG_NS, "polygon")
-    element.setAttributeNS(null, "points", `${formula(coordinates[0],coordinates[2])},${formula(coordinates[1],coordinates[3])} ${coordinates[2]},${coordinates[3]} ${formula(coordinates[2],coordinates[4])},${formula(coordinates[3], coordinates[5])} `)
-    element.setAttributeNS(null, "fill", "black")
+    console.log(coordinates)
     parent.setAttributeNS(null, "fill", "grey")
-    SVG.appendChild(element)
+    let top = triangle(formula(coordinates[0],coordinates[2]), formula(coordinates[1],coordinates[3]), coordinates[2] ,coordinates[3], formula(coordinates[2],coordinates[4]), formula(coordinates[3], coordinates[5]))
+    let left = triangle(coordinates[0],coordinates[1], formula(coordinates[0],coordinates[2]), formula(coordinates[1],coordinates[3]), formula(coordinates[0],coordinates[4]), formula(coordinates[1], coordinates[5]))
+    let right = triangle(formula(coordinates[0],coordinates[4]), formula(coordinates[1], coordinates[5]), formula(coordinates[2],coordinates[4]), formula(coordinates[3],coordinates[5]), coordinates[4], coordinates[5])
     counter++
+    three_triangles(top,counter, count)
+    three_triangles(left,counter, count)
+    three_triangles(right,counter, count)
+}
 
-    //topTriangle(element,counter,count)
+function triangle(x1,y1,x2,y2,x3,y3){
+    let SVG = document.querySelector("svg")
+    let element = document.createElementNS(SVG_NS, "polygon")
+    element.setAttributeNS(null, "points", `${x1},${y1} ${x2},${y2} ${x3},${y3}`)
+    element.setAttributeNS(null, "fill", "black")
+    SVG.appendChild(element)
+    return element
+
     
 }
-function leftTriangle(parent,counter,count){
-    //should set parents color to grey 
-        if(counter == count + 1){
-            console.log(counter)
-            console.log(count)
-            return 
-        }
-    
-        let SVG = document.querySelector("svg")
-        let points = parent.getAttributeNS(null, "points").split(" ")
-        let coordinates = []
-        console.log(points)
-        
-        for (let index = 0; index < points.length; index++) {
-            let coord = points[index].split(",")
-            coordinates.push(parseInt(coord[0]))
-            coordinates.push(parseInt(coord[1]))
-            
-        }
-    
-        let element = document.createElementNS(SVG_NS, "polygon")
-        element.setAttributeNS(null, "points", `${coordinates[0]},${coordinates[1]} ${formula(coordinates[0],coordinates[2])},${formula(coordinates[1],coordinates[3])} ${formula(coordinates[0],coordinates[4])},${formula(coordinates[1], coordinates[5])} `)
-        element.setAttributeNS(null, "fill", "black")
-        parent.setAttributeNS(null, "fill", "grey")
-        SVG.appendChild(element)
-        counter++
-    
-        //leftTriangle(element,counter,count)
-   
-    }
-
-function rightTriangle(parent,counter,count){
-    //should set parents color to grey 
-        if(counter == count + 1){
-            console.log(counter)
-            console.log(count)
-            return 
-        }
-    
-        let SVG = document.querySelector("svg")
-        let points = parent.getAttributeNS(null, "points").split(" ")
-        let coordinates = []
-        console.log(points)
-        
-        for (let index = 0; index < points.length; index++) {
-            let coord = points[index].split(",")
-            coordinates.push(parseInt(coord[0]))
-            coordinates.push(parseInt(coord[1]))
-            
-        }
-    
-        let element = document.createElementNS(SVG_NS, "polygon")
-        element.setAttributeNS(null, "points", `${formula(coordinates[0],coordinates[4])},${formula(coordinates[1], coordinates[5])} ${formula(coordinates[2],coordinates[4])},${formula(coordinates[3],coordinates[5])} ${coordinates[4]},${coordinates[5]}`)
-        element.setAttributeNS(null, "fill", "black")
-        parent.setAttributeNS(null, "fill", "grey")
-        SVG.appendChild(element)
-        counter++
-        //rightTriangle(element,counter,count)
-    
-    }
-
 function formula(p1, p2){
     return (p1 + p2)/2
 }
+
+//TODO: make figure get larger/smaller
